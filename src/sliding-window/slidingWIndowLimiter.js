808e57086b2f2db52ad41e1f77e53438e545e37b
Aaroh Bhardwaj <aarohb.1507@gmail.com>
2026-02-26 01:14:14 +0530

    refactor: improve sliding window limiter implementation and error handling

--- src/sliding-window/slidingWIndowLimiter.js
+++ src/sliding-window/slidingWIndowLimiter.js
@@ -1,33 +1,32 @@
 import redis from 'ioredis'
 const redisClient = redis()
 
-export function slidingWindowLimiter({maxRequests, windowInSecs})=>{
+export const slidingWindowLimiter = ({windowInSeconds, maxRequests}) => {
     return async function(req, res, next){
         try{
-        const ip = req.ip
-        const key = `sw:${ip}`
-        const now = Date.now()
-        const windowStart = now - windowInSecs*1000
-        await redisClient.zremrangebyscore(key, 0, windowStart)
-
-        const count = redisClient.zcard(key)
-        if(count>maxRequests){
-            return res.status(429).json({
-                Success: "False",
-                timeRemainingForResend: Math.floor((windowInSecs)/1000) - Math.floor((now - windowStart)/1000)
-            })
-        }
-        await redisClient.zadd(key, now, now.toString())
-        await redisClient.zadd(key, windowInSecs)
+            const ip = req.ip
+            const key = `sw:${ip}`
+            const now = Date.now()
+            const windowStart = now - windowInSeconds*1000
+            await redisClient.zremrangebyscore(key,0,windowStart)
+            const count = await redisClient.zcard(key)
+            if(count >= maxRequests){
+                return res.status(429).json({
+                    Success: 'False',
+                    message: 'You have reached the limit for this window',
+                    try_after: windowInSeconds - Math.floor((now - windowStart)/1000)
+                })
+            }
+            await redisClient.zadd(key, now, now.toString())
+            await redisClient.expire(key, windowInSeconds)
+            next()
         }catch(error){
-            console.error(error)
+            console.log(error.message)
             return res.status(500).json({
-                Success: "False",
-                message: "Internal Server Error"
+                message: "The sliding window limiter malfunctioned."
             })
+            next()
         }
     }
 }
-
-
 
